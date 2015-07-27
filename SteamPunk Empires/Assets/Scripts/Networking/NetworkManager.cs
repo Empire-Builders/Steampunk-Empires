98d7e2e4c769c813ec7d7689bb9b3bd6ca52a44e
Gregory Bailey <341gregory@gmail.com>
2015-07-27 21:19:13 +0300

    InitialCommit
    Adding everything done so far

---
+++ SteamPunk Empires/Assets/Scripts/Networking/NetworkManager.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NetworkManager : MonoBehaviour {
+	
+	private const string typeName = "SteampunkEmpires";
+	private HostData[] hostList;
+
+	public Image createServerButton;
+	public Image showServerListButton;
+	public GameObject serverCreatedImage;
+	public string gameName;
+	
+	public void StartServer()
+	{
+		Network.InitializeServer(4, 10012, !Network.HavePublicAddress());
+		MasterServer.RegisterHost(typeName, gameName);
+	}
+	
+	void OnServerInitialized()
+	{
+		Debug.Log("Server Initializied");
+		createServerButton.enabled = false;
+		createServerButton.GetComponentInChildren<Text>().enabled = false;
+		showServerListButton.enabled = false;
+		showServerListButton.GetComponentInChildren<Text>().enabled = false;
+		serverCreatedImage.SetActive(true);
+
+	}
+	
+	public void RefreshHostList()
+	{
+		MasterServer.RequestHostList(typeName);
+	}
+	
+	void OnMasterServerEvent(MasterServerEvent msEvent)
+	{
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList();
+	}
+	
+	private void JoinServer(HostData hostData)
+	{
+		Network.Connect(hostData);
+	}
+	
+	void OnConnectedToServer()
+	{
+		Debug.Log("Server Joined");
+	}
+}
