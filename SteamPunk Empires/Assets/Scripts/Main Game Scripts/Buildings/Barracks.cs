b83475a0c2daf2ec4f2ab548a2e8d55e82cf4ae5
Gregory Bailey <341gregory@gmail.com>
2015-08-06 09:23:46 +0300

    created map
    now have a basic village set up, so that work can begin on some graphics

---
+++ SteamPunk Empires/Assets/Scripts/Main Game Scripts/Buildings/Barracks.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Barracks : Building {
+		
+	public Barracks(int level)
+	{
+		Level = level;
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ SteamPunk Empires/Assets/Scripts/Main Game Scripts/Village.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Village : MonoBehaviour{
+
+	public Headquarters headquarters;
+	public Barracks barracks;
+	public GoldPit goldPit;
+
+	void Start()
+	{
+		Headquarters headquarters = new Headquarters(1);
+		Barracks barracks = new Barracks (1);
+		GoldPit goldPit = new GoldPit (1);
+	}
+
+
+	public int points()
+	{
+		return headquarters.Level + goldPit.Level + barracks.Level;
+	}
+
+
+}
